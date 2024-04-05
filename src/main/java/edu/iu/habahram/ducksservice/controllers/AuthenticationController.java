6b926bcba94c87b6e8716ca1f693c62e5381c0d0
elijah7000 <143031360+Elijah-J-Kim@users.noreply.github.com>
2024-04-05 13:39:45 -0400

    Initial commit

---
+++ src/main/java/edu/iu/habahram/ducksservice/controllers/AuthenticationController.java
@@ -0,0 +1,46 @@
+package edu.iu.habahram.ducksservice.controllers;
+
+import edu.iu.habahram.ducksservice.model.Customer;
+import edu.iu.habahram.ducksservice.repository.CustomerRepository;
+import edu.iu.habahram.ducksservice.security.TokenService;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.Authentication;
+
+@RestController
+@CrossOrigin
+public class AuthenticationController {
+    CustomerRepository customerRepository;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+
+    public AuthenticationController(CustomerRepository
+                                            customerRepository,
+                                    AuthenticationManager authenticationManager,
+                                    TokenService tokenService) {
+        this.customerRepository = customerRepository;
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+    }
+    @PostMapping("/signup")
+    public void signup(@RequestBody Customer customer) {
+        try {
+            customerRepository.save(customer);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PostMapping("/signin")
+    public String login(@RequestBody Customer customer) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        customer.username(),
+                        customer.password()
+                )
+        );
+        return tokenService.generateToken(authentication);
+    }
+}
